8b089715c508f10f0c286e822d4bf3106a19dbf5
Tyler Pierce <ecreipt@gmail.com>
2025-11-18 01:12:20 -0700

    Added Toast Pop-ups and storage, presenting data in stats page

---
+++ Conversion Counter/ToastView.swift
@@ -0,0 +1,18 @@
+//
+//  ToastView.swift
+//  Conversion Counter
+//
+//  Created by Tyler Pierce on 11/16/25.
+//
+
+import SwiftUI
+
+struct ToastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToastView()
+}
